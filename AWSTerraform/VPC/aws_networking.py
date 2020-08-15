a1e07297ac2dfd6e45e3b4369086390855540adb
lucabem <luiscaman98@gmail.com>
2020-08-15 12:10:28 +0200

    Added new objects: EIP, NAT_Gateway && Internet_Gateway.

--- AWSTerraform/VPC/aws_networking.py
+++ AWSTerraform/VPC/aws_networking.py
@@ -65,7 +65,6 @@ class AWSSubnet(AWSComponent):
         self.map_public_ip_on_launch = map_public_ip_on_launch
         self.outpost_arn = outpost_arn
         self.assign_ipv6_address_on_creation = assign_ipv6_address_on_creation
-        self.tags = tags
 
     def to_terraform(self):
         resource_header = "aws_subnet"
@@ -106,5 +105,153 @@ class AWSSubnet(AWSComponent):
         return ".".join(['aws_subnet', self.component_name, 'arn'])
 
 
+class AWSInternetGateway(AWSComponent):
+
+    def __init__(self, component_name, vpc, tags=None):
+        super(AWSInternetGateway, self).__init__(component_name, tags)
+        self.vpc = vpc
+
+    def to_terraform(self):
+        resource_header = "aws_internet_gateway"
+
+        dict_attr = self.__dict__
+        dict_as_tf = ""
+        component_name = '"' + self.component_name + '"'
+
+        for key in dict_attr.keys():
+            if key == 'component_name' or key == 'tags':
+                continue
+            value = dict_attr[key]
+            if value is not None:
+                value = self.vpc.get_id()
+                dict_as_tf += '\t' + key + ' = ' + str(value) + '\n'
+
+        dict_tags_str = self.print_tags()
+
+        dict_as_tf += dict_tags_str
+        string_resource = " ".join(["resource",
+                                    resource_header,
+                                    component_name,
+                                    "{\n",
+                                    dict_as_tf,
+                                    "}"])
+        return string_resource
+
+    def get_id(self):
+        return ".".join(['aws_internet_gateway', self.component_name, 'id'])
+
+    def get_arn(self):
+        return ".".join(['aws_internet_gateway', self.component_name, 'arn'])
+
+
+class AWSEip(AWSComponent):
+
+    def __init__(self, component_name, vpc=None, instance=None, network_interface=None, tags=None):
+        super(AWSEip, self).__init__(component_name, tags)
+        self.vpc = vpc
+        self.instance = instance
+        self.network_interface = network_interface
+
+    def to_terraform(self):
+        resource_header = "aws_eip"
+
+        dict_attr = self.__dict__
+        dict_as_tf = ""
+        component_name = '"' + self.component_name + '"'
+
+        for key in dict_attr.keys():
+            if key == 'component_name' or key == 'tags':
+                continue
+            value = dict_attr[key]
+            if value is not None:
+                if type(value) is bool:
+                    dict_as_tf += '\t' + key + ' = ' + str(value).lower() + '\n'
+                else:
+                    dict_as_tf += '\t' + key + ' = "' + str(value) + '"\n'
+
+        dict_tags_str = self.print_tags()
+
+        dict_as_tf += dict_tags_str
+        string_resource = " ".join(["resource",
+                                    resource_header,
+                                    component_name,
+                                    "{\n",
+                                    dict_as_tf,
+                                    "}"])
+        return string_resource
+
+    def get_id(self):
+        return ".".join(['aws_eip', self.component_name, 'id'])
+
+    def get_private_ip(self):
+        if self.vpc:
+            return ".".join(['aws_eip', self.component_name, 'private_ip'])
+        return None
+
+    def get_private_dns(self):
+        if self.vpc:
+            return ".".join(['aws_eip', self.component_name, 'private_dns'])
+        return None
+
+    def get_public_ip(self):
+        if self.vpc:
+            return ".".join(['aws_eip', self.component_name, 'public_ip'])
+        return None
+
+    def get_public_dns(self):
+        if self.vpc:
+            return ".".join(['aws_eip', self.component_name, 'public_dns'])
+        return None
+
+class AWSNATGateway(AWSComponent):
+
+    def __init__(self, component_name, eip, subnet, tags=None):
+        super(AWSNATGateway, self).__init__(component_name, tags)
+        self.eip = eip
+        self.subnet = subnet
+
+    def to_terraform(self):
+        resource_header = "aws_nat_gateway"
+
+        dict_attr = self.__dict__
+        dict_as_tf = ""
+        component_name = '"' + self.component_name + '"'
+
+        for key in dict_attr.keys():
+            if key == 'component_name' or key == 'tags':
+                continue
+            value = dict_attr[key]
+            if value is not None:
+                value = value.get_id()
+                dict_as_tf += '\t' + key + ' = ' + str(value) + '\n'
+
+        dict_tags_str = self.print_tags()
+
+        dict_as_tf += dict_tags_str
+        string_resource = " ".join(["resource",
+                                    resource_header,
+                                    component_name,
+                                    "{\n",
+                                    dict_as_tf,
+                                    "}"])
+        return string_resource
+
+    def get_id(self):
+        return ".".join(['aws_nat_gateway', self.component_name, 'id'])
+
+    def get_allocation_id(self):
+        return ".".join(['aws_nat_gateway', self.component_name, 'allocation_id'])
+
+    def get_subnet_id(self):
+        return ".".join(['aws_nat_gateway', self.component_name, 'subnet_id'])
+
+    def get_private_ip(self):
+        return ".".join(['aws_nat_gateway', self.component_name, 'private_ip'])
+
+    def get_public_ip(self):
+        return ".".join(['aws_nat_gateway', self.component_name, 'public_ip'])
+
+    def get_network_interface_id(self):
+        return ".".join(['aws_nat_gateway', self.component_name, 'network_interface_id'])
 
 
--- __main__.py
+++ __main__.py
@@ -8,4 +8,11 @@ if __name__ == "__main__":
     aws_subnet = AWSSubnet(cidr_block='172.10.10.1/24', component_name='subnet_luis_1', vpc=aws_vpc,
                            tags={'Name': 'TFG', 'Billing': 'Test'})
 
-    print(aws_subnet.get_arn())
+    aws_internet_gateway = AWSInternetGateway("ig_luis", aws_vpc, tags={'Name': 'TFG', 'Billing': 'Test'})
+
+    aws_eip = AWSEip("eipalloc-luis", vpc=True)
+
+    aws_nat_gateway = AWSNATGateway("nat_luis", aws_eip, aws_subnet, tags={'Name': 'TFG', 'Billing': 'Test'})
+
+    print (aws_nat_gateway.to_terraform())
+
